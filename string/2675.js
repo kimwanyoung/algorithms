5f6c60ad303bba9add7e70fda3477a30dd9e8429
kimwanyoung <dhks2869@gmail.com>
2023-04-08 19:37:07 +0900

    백준 알고리즘 : 2675

---
+++ string/2675.js
@@ -0,0 +1,14 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
+
+let testCase = Number(input[0])
+
+for(let i = 1; i <= testCase; i++){
+    let [r, s] = input[i].split(' ')
+    let result = ''
+
+    for(let j = 0; j <= s.length; j++){
+        result += s.charAt(j).repeat(r)
+    }
+
+    console.log(result)
+}
