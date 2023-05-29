35ef8b082e89be1898a1510ef4446a39196e2b94
kimwanyoung <dhks2869@gmail.com>
2023-05-29 16:27:54 +0900

    feat : 프로그래머스 비밀지도 lv.1 수정

--- 프그래머스/secretMap.js
+++ 프그래머스/secretMap.js
@@ -1,36 +1,12 @@
-const binaryArr = (n, arr) => {
-  let binarys = [];
-  for (let i = 0; i < arr.length; i++) {
-    binarys.push(arr[i].toString(2).split("").join(""));
-  }
-  for (let i = 0; i < binarys.length; i++) {
-    if (binarys[i].length < n) {
-      let zeros = new Array(n - binarys[i].length).fill("0");
-      binarys[i] = [...zeros, ...binarys[i]].join("");
-    }
-  }
-  return binarys;
-};
-
-const secretMap = (str1, str2) => {
-  const result = [];
-  for (let i = 0; i < str1.length; i++) {
-    if (str1[i] + str2[i] > 0) {
-      result.push("#");
-    } else {
-      result.push(" ");
-    }
-  }
-  return result.join("");
-};
-
 function solution(n, arr1, arr2) {
-  var answer = [];
-  const binary1 = binaryArr(n, arr1);
-  const binary2 = binaryArr(n, arr2);
+  let binaryMap = arr1.map((v, i) =>
+    addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, (a) =>
+      +a ? "#" : " "
+    )
+  );
+  return binaryMap;
+}
 
-  for (let i = 0; i < n; i++) {
-    answer.push(secretMap(binary1[i], binary2[i]));
-  }
-  return answer;
+function addZero(n, s) {
+  return "0".repeat(n - s.length) + s;
 }
