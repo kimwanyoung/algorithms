f30dcd1af8911bd326c5e82620f63e591db1b093
kimwanyoung <dhks2869@gmail.com>
2023-07-15 17:55:40 +0900

    feat : 백준 13305 주유소

--- 백준/greedy/13305.js
+++ 백준/greedy/13305.js
@@ -1,18 +1,14 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-const n = Number(input[0])
-const dist = input[1].split(' ').map(Number)
-const cost = input[2].split(' ').map(Number)
+const stationCount = Number(input[0]);
+const distance = input[1].split(" ").map(Number);
+const oilCost = input[2].split(" ").map(Number);
 
-let minCost = cost[0]
-for(let i = 0 ; i < n; i++){
-    minCost = Math.min(minCost, cost[i])
-    cost[i] = minCost
+let totalCost = BigInt(0);
+let minCost = oilCost[0];
+for (let i = 0; i < stationCount - 1; i++) {
+  if (minCost > oilCost[i]) minCost = oilCost[i];
+  totalCost += BigInt(minCost) * BigInt(distance[i]);
 }
 
-let answer = BigInt(0)
-for(let i = 0; i < n - 1; i++){
-    answer += BigInt(dist[i]) * BigInt(cost[i])
-}
-
-console.log(String(answer))
+console.log(String(totalCost));
