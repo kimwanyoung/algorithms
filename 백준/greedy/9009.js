dbfacd77c286985134cf4049029fcc5ea48fb1cc
kimwanyoung <dhks2869@gmail.com>
2023-07-16 17:00:14 +0900

    feat : 백준 9009 피보나치

--- 백준/greedy/9009.js
+++ 백준/greedy/9009.js
@@ -1,26 +1,37 @@
-function findFibonacciSum(n) {
-    const fibonacci = [0, 1];
-    let k = 2;
-    while (fibonacci[k - 1] < n) {
-      fibonacci[k] = fibonacci[k - 1] + fibonacci[k - 2];
-      k++;
-    }
-    
-    const result = [];
-    let i = k - 1;
-    while (n > 0 && i >= 0) {
-      if (n >= fibonacci[i]) {
-        result.push(fibonacci[i]);
-        n -= fibonacci[i];
-      }
-      i--;
-    }
-    
-    return result.reverse();
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const fibonacci = (number) => {
+  const fibArray = [0, 1];
+
+  let nextFib;
+  while (true) {
+    nextFib = fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2];
+    if (nextFib > number) break;
+    fibArray.push(nextFib);
   }
-  input.shift()
-  const piboNum = input.map(Number)
-  console.log(piboNum)
-  for(let i = 0 ; i < piboNum.length; i++){
-      console.log(findFibonacciSum(piboNum[i]).join(' '))
+
+  return fibArray;
+};
+
+const numberCount = Number(input[0]);
+const fibNumberList = [];
+for (let i = 1; i <= numberCount; i++) {
+  fibNumberList.push(Number(input[i]));
+}
+
+for (let i = 0; i < fibNumberList.length; i++) {
+  const fibonacciArray = fibonacci(fibNumberList[i]);
+  let sum = 0;
+  let fibString = [];
+  for (let j = fibonacciArray.length - 1; j >= 0; j--) {
+    if (sum >= fibNumberList[i]) {
+      break;
+    }
+    if (sum + fibonacciArray[j] <= fibNumberList[i]) {
+      sum += fibonacciArray[j];
+      fibString.push(fibonacciArray[j]);
+    }
   }
+  fibString.reverse();
+  console.log(fibString.join(" "));
+}
