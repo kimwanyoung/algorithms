e2269011b544eae8d4bf22cbbf363c72cc7b1064
kimwanyoung <dhks2869@gmail.com>
2023-07-18 14:25:52 +0900

    feat : 백준 2805 나무 자르기

--- 백준/binarySearch/2805.js
+++ 백준/binarySearch/2805.js
@@ -1,32 +1,25 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-let trees = input[1].split(' ').map(Number)
-let max = Number(input[0].split(' ')[1])
+const [treeCount, height] = input[0].split(" ").map(Number);
+const trees = input[1].split(" ").map(Number);
 
-let start = 0
-let end = trees.reduce((a, b) => Math.max(a, b))
+let start = 1;
+let end = Math.max(...trees);
 
-let result = 0
-while(start <= end){
-  let mid = parseInt((start + end) / 2)
-  let total = 0
-  for(let i of trees){
-    if(i - mid < 0){
-      total += 0
-    } else {
-      total += (i - mid)
-    }
+let result = 0;
+while (start <= end) {
+  const mid = parseInt((start + end) / 2);
+  let totalCut = 0;
+  for (let tree of trees) {
+    if (tree > mid) totalCut += tree - mid;
   }
-  if(total < max){
-    end = mid - 1
-  } else if(total === max) {
-    result = mid
-    break
+
+  if (totalCut < height) {
+    end = mid - 1;
   } else {
-    result = mid
-    start = mid + 1
+    start = mid + 1;
+    result = mid;
   }
-
 }
 
-console.log(result)
+console.log(result);
