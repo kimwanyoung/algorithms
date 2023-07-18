691866b4f788c36cf6cb86816dd15afb05906a09
kimwanyoung <dhks2869@gmail.com>
2023-07-18 14:09:04 +0900

    feat : 백준 2512 예산

--- 백준/binarySearch/2512.js
+++ 백준/binarySearch/2512.js
@@ -1,26 +1,24 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-let n = Number(input[0].split(' ')[0])
-let arr = input[1].split(' ').map(Number)
-let m = Number(input[2])
+const arr = input[1].split(" ").map(Number);
+const totalCost = Number(input[2]);
 
-let start = 1
-let end = arr.reduce((a, b) => Math.max(a,b))
+let start = 1;
+let end = Math.max(...arr);
 
-let result = 0
-while(start <= end){
-    let mid = parseInt((start + end) / 2)
-    let total = 0
-    for(let x of arr){
-        total += Math.min(mid, x)
-    }
-    if(total <= m){
-        result = mid
-        start = mid + 1
-    } else {
-      end = mid - 1  
-    }
+let result = 0;
+while (start <= end) {
+  let mid = parseInt((start + end) / 2);
+  let total = 0;
+  for (let x of arr) {
+    total += Math.min(mid, x);
+  }
+  if (total <= totalCost) {
+    result = mid;
+    start = mid + 1;
+  } else {
+    end = mid - 1;
+  }
 }
 
-console.log(result)
-
+console.log(result);
