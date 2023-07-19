11ffc07751cb4d7e722876257c56c0e4912b4fd9
kimwanyoung <dhks2869@gmail.com>
2023-07-19 19:46:58 +0900

    feat : 백준 1654 랜선 자르기

--- 백준/binarySearch/1654.js
+++ 백준/binarySearch/1654.js
@@ -1,28 +1,28 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n')
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-const line = Number(input[0].split(' ')[0])
-const lineNumber = Number(input[0].split(' ')[1])
-let lines = []
-for(let i = 1; i <= line; i++){
-    lines.push(Number(input[i]))
+const [k, n] = input[0].split(" ").map(Number);
+
+const lines = [];
+for (let i = 1; i <= k; i++) lines.push(Number(input[i]));
+
+let start = 1;
+let end = Math.max(...lines);
+
+let result = 0;
+while (start <= end) {
+  let sum = 0;
+  const mid = parseInt((start + end) / 2);
+  for (let x of lines) {
+    const line = parseInt(x / mid);
+    sum += line;
+  }
+
+  if (sum >= n) {
+    start = mid + 1;
+    result = mid;
+  } else {
+    end = mid - 1;
+  }
 }
 
-let start = 0
-let end = lines.reduce((a, b) => Math.max(a, b))
-
-let result = 0
-while(start <= end){
-  let mid = parseInt((start + end) / 2)
-  let sum = 0
-  for(let i of lines){
-    sum += parseInt(i / mid)
-  }
-  if(sum < lineNumber){
-    end = mid - 1
-  }else {
-     result = mid
-    start = mid + 1
-  }
-}
-
-console.log(result)
+console.log(result);
