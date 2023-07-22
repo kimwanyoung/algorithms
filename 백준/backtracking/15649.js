009ffff78cd5f7c4d1268e3c64fdfa38743498a8
kimwanyoung <dhks2869@gmail.com>
2023-07-22 22:47:36 +0900

    feat : 백준 15649 n과m(1)

--- 백준/backtracking/15649.js
+++ 백준/backtracking/15649.js
@@ -1,29 +1,29 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split(' ')
+const input = require("fs").readFileSync("/dev/stdin").toString();
 
-let [n, m] = input.map(Number)
-let arr = []
-for(let i = 1; i<= n; i++) arr.push(i)
-let visited = new Array(n).fill(false)
-let selected = []
+let [n, m] = input.split(" ").map(Number);
+let arr = [];
+for (let i = 1; i <= n; i++) arr.push(i);
+let visited = new Array(n).fill(false);
+let selected = [];
 
-let answer = ''
-const dfs = (arr, depth) => {
-  if(depth === m){
-    let result = []
-    for(let i of selected) result.push(i)
-    for(let x of result) answer += x + " "
-    answer += "\n"
-    return
+let answer = "";
+function dfs(arr, depth) {
+  if (depth == m) {
+    let result = [];
+    for (let i of selected) result.push(arr[i]);
+    for (let x of result) answer += x + " ";
+    answer += "\n";
+    return;
   }
-  for(let i = 1; i <= arr.length; i++){
-    if(visited[i]) continue
-    selected.push(i)
-    visited[i] = true
-    dfs(arr, depth + 1)
-    selected.pop()
-    visited[i] = false
+  for (let i = 0; i < arr.length; i++) {
+    if (visited[i]) continue;
+    selected.push(i);
+    visited[i] = true;
+    dfs(arr, depth + 1);
+    selected.pop();
+    visited[i] = false;
   }
 }
 
-dfs(arr, 0)
-console.log(answer)
+dfs(arr, 0);
+console.log(answer);
