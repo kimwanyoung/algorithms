652db6da6fa68cc8cacd1272934d9d5a9d21d035
kimwanyoung <dhks2869@gmail.com>
2023-07-23 05:05:32 +0900

    feat : 백준 10974 모든 순열

--- 백준/backtracking/10974.js
+++ 백준/backtracking/10974.js
@@ -1,30 +1,28 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+const input = require("fs").readFileSync("/dev/stdin").toString();
 
-const n = Number(input[0]);
+const n = Number(input);
 const arr = [];
 for (let i = 1; i <= n; i++) arr.push(i);
 const visited = new Array(n).fill(false);
 const selected = [];
 
 let answer = "";
-const dfs = (arr, depth) => {
+function dfs(arr, depth) {
   if (depth === n) {
     const result = [];
-    for (let i of selected) result.push(arr[i]);
-    for (let x of result) answer += x + " ";
+    for (const i of selected) result.push(arr[i]);
+    for (const x of result) answer += x + " ";
     answer += "\n";
-    return;
   }
-
   for (let i = 0; i < arr.length; i++) {
     if (visited[i]) continue;
     selected.push(i);
     visited[i] = true;
     dfs(arr, depth + 1);
-    selected.pop();
     visited[i] = false;
+    selected.pop();
   }
-};
+}
 
 dfs(arr, 0);
 console.log(answer);
