fce420cc51b69017c6bf81014340756b955215b7
kimwanyoung <dhks2869@gmail.com>
2023-07-28 02:24:28 +0900

    feat : 백준 2606 바이러스

--- 백준/dfs/2606.js
+++ 백준/dfs/2606.js
@@ -1,25 +1,27 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-const n = Number(input[0]);
-const m = Number(input[1]);
+const node = Number(input[0]);
+const edge = Number(input[1]);
+
+const visited = Array(node + 1).fill(false);
 const graph = [];
-for (let i = 1; i <= n; i++) graph[i] = [];
-for (let i = 2; i <= m + 1; i++) {
+for (let i = 0; i <= node; i++) graph.push([]);
+for (let i = 2; i <= edge + 1; i++) {
   const [x, y] = input[i].split(" ").map(Number);
   graph[x].push(y);
   graph[y].push(x);
 }
 
-let cnt = 0;
-const visited = new Array(n + 1).fill(false);
-
-const dfs = (x) => {
-  visited[x] = true;
-  cnt++;
-  for (const y of graph[x]) {
-    if (!visited[y]) dfs(y);
+let count = 0;
+const dfs = (graph, visited, node) => {
+  visited[node] = true;
+  for (const y of graph[node]) {
+    if (!visited[y]) {
+      dfs(graph, visited, y);
+      count += 1;
+    }
   }
 };
 
-dfs(1);
-console.log(cnt - 1);
+dfs(graph, visited, 1);
+console.log(count);
