4fb0a426048e69d8b24413c51751ffca9c0dbbf5
kimwanyoung <dhks2869@gmail.com>
2023-07-29 02:31:14 +0900

    feat : 백준 1240 노드사이의 거리

--- 백준/dfs/1240.js
+++ 백준/dfs/1240.js
@@ -1,27 +1,28 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-const [n, m] = input[0].split(" ").map(Number);
+const [node, pair] = input[0].split(" ").map(Number);
+
 const graph = [];
-let visited = new Array(n + 1).fill(false);
-let distance = new Array(n + 1).fill(-1);
-for (let i = 1; i <= n; i++) graph[i] = [];
-for (let i = 1; i < n; i++) {
-  const [x, y, cost] = input[i].split(" ").map(Number);
-  graph[x].push([y, cost]);
-  graph[y].push([x, cost]);
+for (let i = 1; i <= node; i++) graph[i] = [];
+for (let i = 1; i < node; i++) {
+  const [x, y, distance] = input[i].split(" ").map(Number);
+  graph[x].push([y, distance]);
+  graph[y].push([x, distance]);
 }
 
-const dfs = (x, dist) => {
-  if (visited[x]) return;
-  visited[x] = true;
-  distance[x] = dist;
-  for (const [y, cost] of graph[x]) dfs(y, dist + cost);
-};
+for (let i = 0; i < pair; i++) {
+  const [x, y] = input[node + i].split(" ").map(Number);
 
-for (let i = 0; i < m; i++) {
-  const [x, y] = input[n + i].split(" ").map(Number);
-  visited = new Array(n + 1).fill(false);
-  distance = new Array(n + 1).fill(-1);
+  let visited = new Array(pair + 1).fill(false);
+  let distance = new Array(pair + 1).fill(-1);
+
+  const dfs = (x, dist) => {
+    if (visited[x]) return;
+    visited[x] = true;
+    distance[x] = dist;
+    for (const [y, cost] of graph[x]) dfs(y, dist + cost);
+  };
+
   dfs(x, 0);
   console.log(distance[y]);
 }
