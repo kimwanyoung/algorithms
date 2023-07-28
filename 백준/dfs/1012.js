917a0c88a6c8b6dea521a0aa9345fd82644f9b39
kimwanyoung <dhks2869@gmail.com>
2023-07-28 12:06:54 +0900

    feat : 백준 1012 유기농배추

--- 백준/dfs/1012.js
+++ 백준/dfs/1012.js
@@ -1,38 +1,42 @@
-const dfs = (graph, n, m, x, y) => {
-  if (x <= -1 || x >= n || y <= -1 || y >= m) return false;
-  if (graph[x][y] === 1) {
-    graph[x][y] = -1;
-    dfs(graph, n, m, x - 1, y);
-    dfs(graph, n, m, x, y - 1);
-    dfs(graph, n, m, x + 1, y);
-    dfs(graph, n, m, x, y + 1);
-    return true;
-  }
-  return false;
-};
-
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
 let testCase = Number(input[0]);
 
-let line = 1;
+let currentTestCase = 1;
 while (testCase--) {
-  let [m, n, k] = input[line].split(" ").map(Number);
+  const [m, n, k] = input[currentTestCase].split(" ").map(Number);
   const graph = [];
-  for (let i = 0; i < n; i++) {
-    graph[i] = new Array(m);
+  let count = 0;
+
+  const dfs = (graph, x, y) => {
+    if (x >= n || y >= m || x < 0 || y < 0) return false;
+    if (graph[x][y] === 1) {
+      graph[x][y] = -1;
+
+      dfs(graph, x - 1, y);
+      dfs(graph, x, y - 1);
+      dfs(graph, x + 1, y);
+      dfs(graph, x, y + 1);
+      return true;
+    }
+
+    return false;
+  };
+
+  for (let j = 0; j < n; j++) {
+    graph[j] = new Array(m);
   }
-  for (let i = 1; i <= k; i++) {
-    let [y, x] = input[line + i].split(" ").map(Number);
+
+  for (let j = 1; j <= k; j++) {
+    const [y, x] = input[currentTestCase + j].split(" ").map(Number);
     graph[x][y] = 1;
   }
 
-  let answer = 0;
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (dfs(graph, n, m, i, j)) answer++;
+  for (let j = 0; j < n; j++) {
+    for (let x = 0; x < m; x++) {
+      if (dfs(graph, j, x)) count++;
     }
   }
-  line += k + 1;
-  console.log(answer);
+  console.log(count);
+  currentTestCase += k + 1;
 }
