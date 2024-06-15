2e2a945e42ed404346e5735e34f582a2ec3f875f
kimwanyoung <dhks2869@gmail.com>
2024-06-15 18:54:04 +0900

    feat  :구름 gamejam 재풀이

--- 구름/GameJam/solution.js
+++ 구름/GameJam/solution.js
@@ -1,77 +1,78 @@
 const readline = require('readline');
 
-// (async () => {
-//     let rl = readline.createInterface({ input: process.stdin });
-//
-//     const input = [];
-//     for await (const line of rl) {
-//         input.push(line);
-//         rl.close();
-//     }
-//
-//     const [boardSize, goormPosition, playerPosition, ...commands] = input;
-//     solution(Number(boardSize), goormPosition, playerPosition, commands);
-//
-//     process.exit();
-// })();
+(async () => {
+    let rl = readline.createInterface({ input: process.stdin });
+
+    const input = [];
+    for await (const line of rl) {
+        input.push(line);
+    }
+    rl.close();
+
+    const [boardSize, goormPosition, playerPosition, ...commands] = input;
+    solution(Number(boardSize), goormPosition, playerPosition, commands);
+
+    process.exit();
+})();
 
 function solution(boardSize, goormPosition, playerPosition, commands) {
-    const goormBoard = createBoard(+boardSize);
-    const playerBoard = createBoard(+boardSize);
-    const commandBoard = commands.map((command) => command.split(" "));
+    const goormBoard = initializeBoard(boardSize);
+    const playerBoard = initializeBoard(boardSize);
 
-    const [gy, gx] = goormPosition.split(" ").map(Number);
-    const [py, px] = playerPosition.split(" ").map(Number);
+    goormPosition = goormPosition.split(" ").map((p) => p - 1);
+    playerPosition = playerPosition.split(" ").map((p) => p - 1);
 
-    const goormScore = playGame(gy - 1, gx - 1, goormBoard, commandBoard);
-    const playerScore = playGame(py - 1, px - 1, playerBoard, commandBoard);
+    const goormScore = playGame(goormBoard, goormPosition, commands);
+    const playerScore = playGame(playerBoard, playerPosition, commands);
 
     const winner = goormScore > playerScore ? "goorm" : "player";
-    if(winner === "goorm") console.log(winner + " " + goormScore);
+    if(winner === "goorm") console.log(winner + " " + goormScore)
     else console.log(winner + " " + playerScore);
 }
 
-function createBoard(boardSize) {
-    return Array.from(Array(boardSize), () => new Array(boardSize).fill(false));
-}
+function playGame(board, position, commands) {
+    const gameBoard = board;
+    const direction = {
+        L: [0, -1],
+        R: [0, 1],
+        D: [1, 0],
+        U: [-1, 0],
+    }
 
-function playGame(y, x, board, commands) {
-    const commandType = ["U", "D", "R", "L"];
-    const dy = [1, -1, 0, 0];
-    const dx = [0, 0, 1, -1];
+    let [y, x] = position;
+    commands = commands.map((command) => command.split(" "));
+
 
     let flag = true;
+    let score = 1;
     while(flag) {
-        board[y][x] = true;
+        gameBoard[y][x] = true;
+        const command = commands[y][x].at(-1);
+        const step = commands[y][x].substring(0, commands[y][x].length-1);
 
-        const [count, command] = commands[y][x].split("");
+        for(let i = 0; i < Number(step); i++) {
+            const [dy, dx] = direction[command];
 
-        for(let i = 0; i < Number(count); i++) {
-            const direction = commandType.indexOf(command);
-            y += dy[direction];
-            x += dx[direction];
-
+            y += dy;
+            x += dx;
             if(y > board.length - 1) y = 0;
             if(y < 0) y = board.length - 1;
             if(x > board.length - 1) x = 0;
             if(x < 0) x = board.length - 1;
 
-            if(board[y][x]) {
+            if(!gameBoard[y][x]) {
+                score++;
+                gameBoard[y][x] = true;
+            }else {
                 flag = false;
                 break;
             }
         }
     }
 
-    return board.flat().reduce((cnt, element) => cnt + element, 0);
+    return score;
 }
-const input =["4",
-    "4 2",
-    "2 4",
-    "1L 3D 3L 1U",
-    "2D 2L 4U 1U",
-    "2D 2L 4U 3L",
-    "4D 4D 1R 4R"];
-const [boardSize, goormPosition, playerPosition, ...commands] = input;
 
-solution(boardSize, goormPosition, playerPosition, commands);
+function initializeBoard(boardSize) {
+    return Array.from({length: boardSize}, () => Array.from({length: boardSize}, () => false));
+}
