ab86d631a52eac23ba15077be1dacfe6d7e5b6d6
kimwanyoung <dhks2869@gmail.com>
2024-06-16 16:29:40 +0900

    feat : 구름 계수기 만들기

---
+++ 구름/계수기만들기/solution.js
@@ -0,0 +1,39 @@
+// Run by Node.js
+
+const readline = require("readline");
+
+(async () => {
+    let rl = readline.createInterface({ input: process.stdin });
+    const input = [];
+
+    for await (const line of rl) {
+        input.push(line);
+    }
+    rl.close();
+
+    const [N, max, board, K] = input;
+    solution(N, max, board, K);
+    process.exit();
+})();
+
+function solution(N, max, board, K) {
+    N = Number(N);
+    max = max.split(" ").map(Number);
+    board = board.split(" ").map(Number);
+
+    while(K > 0) {
+        let currentIdx = N - 1;
+        board[currentIdx]++;
+
+        while(board[currentIdx] > max[currentIdx]) {
+            board[currentIdx] = 0;
+            if(currentIdx < 0) break;
+            board[currentIdx - 1]++;
+            currentIdx--;
+        }
+
+        K--;
+    }
+
+    console.log(board.join(""));
+}
