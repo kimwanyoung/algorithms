b10e9df33e6527d5b5f4e189bbbc8e3ddb0d6f77
kimwanyoung <dhks2869@gmail.com>
2024-07-05 16:09:13 +0900

    feat : 백준 정수 삼각형 review

---
+++ 백준/정수삼각형/solution-review.js
@@ -0,0 +1,28 @@
+const input = [
+    "5",
+    "7",
+    "3 8",
+    "8 1 0",
+    "2 7 4 4",
+    "4 5 2 6 5",
+]
+
+const n = Number(input[0]);
+
+const arr = [];
+for(let i = 1; i <= n; i++) {
+    arr.push(input[i].split(" ").map(Number));
+}
+
+for(let i = 1; i < n; i++) {
+    for(let j = 0; j <= i; j++) {
+        let upLeft = 0;
+        if(j !== 0) upLeft = arr[i - 1][j - 1];
+        let upRight = 0;
+        if(j !== i) upRight = arr[i - 1][j];
+
+        arr[i][j] = arr[i][j] + Math.max(upLeft, upRight);
+    }
+}
+
+console.log(Math.max(...arr[n - 1]));
