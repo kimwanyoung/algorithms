6edb522c69667be7483604080b502f072857f4f4
kimwanyoung <dhks2869@gmail.com>
2024-07-05 16:18:23 +0900

    feat : 백준 연속부분최대곱

---
+++ 백준/연속부분최대곱/solution-review.js
@@ -0,0 +1,18 @@
+const input = [
+    "8",
+    "1.1",
+    "0.7",
+    "1.3",
+    "0.9",
+    "1.4",
+    "0.8",
+    "0.7",
+    "1.4"
+].map(Number);
+
+const [n, ...numbers] = input;
+for(let i = 1; i < n; i++) {
+    numbers[i] = Math.max(numbers[i], numbers[i] * numbers[i - 1]);
+}
+
+console.log(Math.max(...numbers).toFixed(3));
