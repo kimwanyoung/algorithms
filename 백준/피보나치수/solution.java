0164d17d5873d8c1825a1f0ba29233c6c80ac7e2
kimwanyoung <dhks2869@gmail.com>
2024-07-23 14:00:38 +0900

    feat : 백준 피보나치 수

---
+++ 백준/피보나치수/solution.java
@@ -0,0 +1,27 @@
+package 백준.피보나치수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class solution {
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
