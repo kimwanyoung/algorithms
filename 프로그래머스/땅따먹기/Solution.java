d6753a7d1873514c2d680476b9dfcd2f46bdac35
kimwanyoung <dhks2869@gmail.com>
2025-03-22 16:13:33 +0900

    feat : 프로그래머스 땅따먹기

---
+++ 프로그래머스/땅따먹기/Solution.java
@@ -0,0 +1,26 @@
+package 프로그래머스.땅따먹기;
+
+public class Solution {
+
+	int solution(int[][] land) {
+		int answer = 0;
+
+		int[][] dp = new int[land.length + 1][4];
+
+		for (int i = 1; i <= land.length; i++) {
+			for (int j = 0; j < 4; j++) {
+				for (int k = 0; k < 4; k++) {
+					if (k == j) {
+						continue;
+					}
+
+					dp[i][j] = Math.max(dp[i][j], land[i - 1][j] + dp[i - 1][k]);
+
+					answer = Math.max(answer, dp[i][j]);
+				}
+			}
+		}
+
+		return answer;
+	}
+}
